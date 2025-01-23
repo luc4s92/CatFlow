2ad46bb5462ef83cefd990a54d54e7b9d629e272
Lucas Gonzalez <luc4s92.g@gmail.com>
2025-01-23 19:18:55 -0300

    Eslint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-4xl text-violet-700 font-black ">
+    <h1 className="text-4xl  font-black text-violet-700 ">
       Welcome to Nextjs 15
     </h1>
   );
